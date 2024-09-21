86808d90c3c5bdec50d88ba92ed657c5ae0f04bb
nicolisartori <115371177+nicolisartori@users.noreply.github.com>
2024-09-20 22:52:59 -0300

    Alteração imagens, script e encomendar
    Inclusos valores dos produtos nas imagens (R$) 
    Script agora muda valor de acordo com o produto que o usuario escolhe
    Pagina encomendar reposicionei alguns itens

--- script1.js
+++ script1.js
@@ -1,3 +1,21 @@
+const imgconforto = '<img src="images/img-encomendar/0101.jpg" width="150px"><img src="images/img-encomendar/0102.jpg" width="150px"><img src="images/img-encomendar/0103.jpg" width="150px">'
+const imgmedio =  '<img src="images/img-encomendar/0201.jpg" width="150px"><img src="images/img-encomendar/0202.jpg" width="150px"><img src="images/img-encomendar/0203.jpg" width="150px">'
+const imgexecutivo =  '<img src="images/img-encomendar/0301.jpg" width="150px"><img src="images/img-encomendar/0302.jpg" width="150px"><img src="images/img-encomendar/0303.jpg" width="150px">'
+
+var escolha = 0
+function change1() {
+    change.innerHTML = imgconforto
+    escolha = 1
+  }
+  function change2() {
+    change.innerHTML = imgmedio
+    escolha = 2
+  }
+  function change3() {
+    change.innerHTML = imgexecutivo
+    escolha = 3
+  }
+
 function valor() {
     let quantidademesas =  document.getElementById('quantidademesas')
     let quantidadecadeiras =  document.getElementById('quantidadecadeiras')
@@ -12,29 +30,40 @@ function valor() {
     finalbancos = valorbancos *3
     respedido = finalbancos + finalcadeiras + finalmesas
 
-    if (valormesas >= 0 && valorcadeiras >= 0 && valorbancos >= 0){
-        resmesas.innerHTML = finalmesas
-        rescadeiras.innerHTML = finalcadeiras
-        resbancos.innerHTML = finalbancos
-        totalpedido.innerHTML = respedido
-    }
-    else {
-        resmesas.innerHTML = `Os valores devem ser maiores que zero`
-        rescadeiras.innerHTML = `Os valores devem ser maiores que zero`
-        resbancos.innerHTML = `Os valores devem ser maiores que zero`
-    }
+    finalmesas1 = valormesas * 9
+    finalcadeiras1 = valorcadeiras * 5
+    finalbancos1 = valorbancos *4
+    respedido1 = finalbancos1 + finalcadeiras1 + finalmesas1
+
+    finalmesas2 = valormesas * 10
+    finalcadeiras2 = valorcadeiras * 6
+    finalbancos2 = valorbancos *5
+    respedido2 = finalbancos2 + finalcadeiras2 + finalmesas2
+
+
+  if (escolha == 1){
+      resmesas.innerHTML = finalmesas
+      rescadeiras.innerHTML = finalcadeiras
+      resbancos.innerHTML = finalbancos
+      totalpedido.innerHTML = respedido
+  }
+  else if (escolha == 2){
+    resmesas.innerHTML = finalmesas1
+    rescadeiras.innerHTML = finalcadeiras1
+    resbancos.innerHTML = finalbancos1
+    totalpedido.innerHTML = respedido1
+
+  } else if (escolha == 3){
+    resmesas.innerHTML = finalmesas2
+      rescadeiras.innerHTML = finalcadeiras2
+      resbancos.innerHTML = finalbancos2
+      totalpedido.innerHTML = respedido2
+
+  } 
+  else {
+      resmesas.innerHTML = `Selecione um produto`
+      rescadeiras.innerHTML = `Selecione um produto`
+      resbancos.innerHTML = `Selecione um produto`
+  }
 }
 
-const imgconforto = '<img src="images/page3_img1.jpg" width="150px"><img src="images/page3_img2.jpg" width="150px"><img src="images/page3_img3.jpg" width="150px">'
-const imgmedio = '<img src="images/page3_img4.jpg" width="150px"><img src="images/page3_img5.jpg" width="150px"><img src="images/page3_img6.jpg" width="150px">'
-const imgexecutivo = '<img src="images/page3_img7.jpg" width="150px"><img src="images/page3_img8.jpg" width="150px"><img src="images/page3_img9.jpg" width="150px">'
-
-function change1() {
-    change.innerHTML = imgconforto
-  }
-  function change2() {
-    change.innerHTML = imgmedio
-  }
-  function change3() {
-    change.innerHTML = imgexecutivo
-  }
