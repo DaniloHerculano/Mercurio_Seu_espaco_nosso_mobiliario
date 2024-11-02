626b54001c1b9646232891b4ef3640ada52b2f3f
Danilo Herculano <101299841+DaniloHerculano@users.noreply.github.com>
2024-11-01 21:48:12 -0300

    Update jquery.main.js
    **jquery.main.js**: Ajustes nas funcionalidades de login, registro e fluxo de navegação.

--- ProjetoMercurio_v0.03/ProjetoMercurio_v0.03/Mercurio/site/login/jquery.main.js
+++ ProjetoMercurio_v0.03/ProjetoMercurio_v0.03/Mercurio/site/login/jquery.main.js
@@ -1,77 +1,63 @@
-//criação de cadastro
+// jquery.main.js
 
-$(function(){
-    //valores de campo originais
+$(function() {
+    // valores de campo originais
     var field_values = {
-            //id        :  value
-            'username'  : 'username',
-            'password'  : 'password',
-            'cpassword' : 'password',
-            'firstname'  : 'first name',
-            'lastname'  : 'last name',
-            'email'  : 'email address'
+        'username'  : 'username',
+        'password'  : 'password',
+        'cpassword' : 'password',
+        'firstname' : 'first name',
+        'lastname'  : 'last name',
+        'email'     : 'email address'
     };
 
-
-    //inputfocus
+    // inputfocus
     $('input#username').inputfocus({ value: field_values['username'] });
     $('input#password').inputfocus({ value: field_values['password'] });
     $('input#cpassword').inputfocus({ value: field_values['cpassword'] }); 
     $('input#lastname').inputfocus({ value: field_values['lastname'] });
     $('input#firstname').inputfocus({ value: field_values['firstname'] });
-    $('input#email').inputfocus({ value: field_values['email'] }); 
+    $('input#email').inputfocus({ value: field_values['email'] });
 
-
-
-
-    //redefinir barra de progresso
+    // redefinir barra de progresso
     $('#progress').css('width','0');
     $('#progress_text').html('0% Completo');
 
-    //primeiro passo
+    // primeiro passo
     $('form').submit(function(){ return false; });
     $('#submit_first').click(function(){
-        //remover classes
         $('#first_step input').removeClass('error').removeClass('valid');
 
-        //verificar se as entradas não estão vazias
         var fields = $('#first_step input[type=text], #first_step input[type=password]');
         var error = 0;
         fields.each(function(){
             var value = $(this).val();
-            if( value.length<4 || value==field_values[$(this).attr('id')] ) {
+            if( value.length < 4 || value == field_values[$(this).attr('id')] ) {
                 $(this).addClass('error');
                 $(this).effect("shake", { times:3 }, 50);
-                
                 error++;
             } else {
                 $(this).addClass('valid');
             }
-        });        
-        
+        });
+
         if(!error) {
             if( $('#password').val() != $('#cpassword').val() ) {
-                    $('#first_step input[type=password]').each(function(){
-                        $(this).removeClass('valid').addClass('error');
-                        $(this).effect("shake", { times:3 }, 50);
-                    });
-                    
-                    return false;
-            } else {   
-                //atualizar barra de progresso
+                $('#first_step input[type=password]').each(function(){
+                    $(this).removeClass('valid').addClass('error');
+                    $(this).effect("shake", { times:3 }, 50);
+                });
+                return false;
+            } else {
                 $('#progress_text').html('33% Completo');
                 $('#progress').css('width','113px');
-                
-                //passos de deslizamento
                 $('#first_step').slideUp();
-                $('#second_step').slideDown();     
-            }               
+                $('#second_step').slideDown();
+            }
         } else return false;
     });
 
-
     $('#submit_second').click(function(){
-        //remover classes
         $('#second_step input').removeClass('error').removeClass('valid');
 
         var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
@@ -79,10 +65,10 @@ $(function(){
         var error = 0;
         fields.each(function(){
             var value = $(this).val();
-            if( value.length<1 || value==field_values[$(this).attr('id')] || ( $(this).attr('id')=='email' && !emailPattern.test(value) ) ) {
+            if( value.length < 1 || value == field_values[$(this).attr('id')] || 
+                ( $(this).attr('id') == 'email' && !emailPattern.test(value) ) ) {
                 $(this).addClass('error');
                 $(this).effect("shake", { times:3 }, 50);
-                
                 error++;
             } else {
                 $(this).addClass('valid');
@@ -90,25 +76,18 @@ $(function(){
         });
 
         if(!error) {
-                //atualizar barra de progresso
-                $('#progress_text').html('66% Completo');
-                $('#progress').css('width','226px');
-                
-                //passos de deslizamento
-                $('#second_step').slideUp();
-                $('#third_step').slideDown();     
+            $('#progress_text').html('66% Completo');
+            $('#progress').css('width','226px');
+            $('#second_step').slideUp();
+            $('#third_step').slideDown();
         } else return false;
-
     });
 
-
     $('#submit_third').click(function(){
-        //atualizar barra de progresso
         $('#progress_text').html('100% Completo');
         $('#progress').css('width','339px');
 
-        //preparar para quarto passo
-        var fields = new Array(
+        var fields = [
             $('#username').val(),
             $('#password').val(),
             $('#email').val(),
@@ -116,23 +95,49 @@ $(function(){
             $('#age').val(),
             $('#gender').val(),
             $('#country').val()                       
-        );
+        ];
+
         var tr = $('#fourth_step tr');
         tr.each(function(){
-            //alert( fields[$(this).index()] )
             $(this).children('td:nth-child(2)').html(fields[$(this).index()]);
         });
-                
-        //passos de deslizamento
+
         $('#third_step').slideUp();
-        $('#fourth_step').slideDown();            
+        $('#fourth_step').slideDown();
     });
 
-
-    $('#submit_fourth').click(function(){
-        //enviar informações para o servidor
-        alert('Data sent');
-        window.location.href="index.html";
+    $('#submit_fourth').click(function() {
+        var userData = {
+            username: $('#username').val(),
+            password: $('#password').val(),
+            email: $('#email').val(),
+            firstname: $('#firstname').val(),
+            lastname: $('#lastname').val(),
+            age: $('#age').val(),
+            gender: $('#gender').val(),
+            country: $('#country').val()
+        };
+    
+        // Salva `userData` no Local Storage
+        localStorage.setItem(userData.email, JSON.stringify(userData));
+        alert('Cadastro realizado com sucesso!');
+        window.location.href = "login.html"; // Redireciona para a página de login
     });
 
+    // Função Esqueci minha senha
+    $('#forgot-password').click(function(e) {
+        e.preventDefault();
+        $('#password-recovery-modal').show();
+    });
+
+    $('#recover-password').click(function() {
+        const recoveryEmail = $('#recovery-email').val();
+        const userData = JSON.parse(localStorage.getItem(recoveryEmail));
+
+        if (userData && userData.password) {
+            $('#recovery-message').text(`Sua senha é: ${userData.password}`);
+        } else {
+            $('#recovery-message').text("E-mail não encontrado. Verifique e tente novamente.");
+        }
+    });
 });
